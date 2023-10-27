b41e58be3cc87d85babbf006eb7c346445f4cccb
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2023-10-27 07:50:23 +0300

    feat: add generating quotes and not repeating same quotes twice in row

--- Random Quotes/main.js
+++ Random Quotes/main.js
@@ -1,32 +1,37 @@
-window.addEventListener('load', function () {
-  const quotes = [
-    'Что разум человека может постигнуть и во что он может поверить, того он способен достичь ~Наполеон Хилл, журналист и писатель ',
-    'Стремитесь не к успеху, а к ценностям, которые он дает ~Альберт Эйнштейн',
-    'Своим успехом я обязана тому, что никогда не оправдывалась и не принимала оправданий от других. ~Флоренс Найтингейл',
-    'За свою карьеру я пропустил более 9000 бросков, проиграл почти 300 игр. 26 раз мне доверяли сделать финальный победный бросок, и я промахивался. Я терпел поражения снова, и снова, и снова. И именно поэтому я добился успеха. ~Майкл Джордан',
-    'Сложнее всего начать действовать, все остальное зависит только от упорства. ~Амелия Эрхарт',
-    'Через 20 лет вы будете больше разочарованы теми вещами, которые вы не делали, чем теми, которые вы сделали. Так отчальте от тихой пристани. Почувствуйте попутный ветер в вашем парусе. Двигайтесь вперед, действуйте, открывайте! ~Марк Твен',
-    'Начинать всегда стоит с того, что сеет сомнения. ~Борис Стругацкий',
-  ];
-  const btnQuote = document.querySelector('#btnQuote');
-  const quotesLen = quotes.length - 1;
+window.addEventListener('load', () => {
+  const quotes = {
+    '~ Альберт Эйнштейн':
+      'Что разум человека может постигнуть и во что он может поверить, того он способен достичь ~Наполеон Хилл, журналист и писатель',
+    '~ Великий человек': 'Стремитесь не к успеху, а к ценностям, которые он дает',
+    '~ Флоренс Найтингейл':
+      'Своим успехом я обязана тому, что никогда не оправдывалась и не принимала оправданий от других.',
+    '~ Майкл Джордан':
+      'За свою карьеру я пропустил более 9000 бросков, проиграл почти 300 игр. 26 раз мне доверяли сделать финальный победный бросок, и я промахивался. Я терпел поражения снова, и снова, и снова. И именно поэтому я добился успеха.',
+    '~ Амелия Эрхарт':
+      'Сложнее всего начать действовать, все остальное зависит только от упорства.',
+    '~ Марк Твен':
+      'Через 20 лет вы будете больше разочарованы теми вещами, которые вы не делали, чем теми, которые вы сделали. Так отчальте от тихой пристани. Почувствуйте попутный ветер в вашем парусе. Двигайтесь вперед, действуйте, открывайте!',
+    '~ Борис Стругацкий': 'Начинать всегда стоит с того, что сеет сомнения.',
+  };
 
-  const quoteText = document.querySelector('#quoteText');
-  let prevRandomNumber = -1;
+  let previousQuote = null; // Инициализируем предыдущую цитату как null
 
-  function randomNum() {
-    let quotesNum = Math.round(Math.random() * quotesLen);
+  const btnQuotesShow = document.querySelector('.btn-info');
+  btnQuotesShow.addEventListener('click', () => {
+    let authors = Object.keys(quotes);
+    let author = authors[Math.floor(Math.random() * authors.length)];
+    let quote = quotes[author];
 
-    if (prevRandomNumber == quotesNum && quotesNum == quotesLen - 1) quotesNum -= 1;
-    else if (prevRandomNumber == quotesNum && quotesNum == 0) quotesNum += 1;
-    else quotesNum += 1;
+    // Проверяем, чтобы новая цитата не совпадала с предыдущей
+    while (quote === previousQuote) {
+      author = authors[Math.floor(Math.random() * authors.length)];
+      quote = quotes[author];
+    }
 
-    console.log(quotesNum);
-    prevRandomNumber = quotesNum;
-    return quotesNum;
-  }
-
-  btnQuote.addEventListener('click', () => {
-    quoteText.innerHTML = quotes[randomNum()];
+    // Обновляем предыдущую цитату
+    previousQuote = quote;
+    console.log(author);
+    document.getElementById('quote').innerHTML = quote;
+    document.getElementById('author').innerHTML = author;
   });
 });
