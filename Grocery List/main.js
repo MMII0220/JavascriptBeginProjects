467ebe0d3885b5f24dbcc7b06f4286bfb85707c9
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2023-11-01 10:55:16 +0300

    refactor: make different fucntions

--- Grocery List/main.js
+++ Grocery List/main.js
@@ -1,22 +1,32 @@
 window.addEventListener('DOMContentLoaded', () => {
   const input = document.querySelector('#userInput');
   const myListTasks = document.querySelector('#groceryItems');
+  const eraser = document.querySelector('#eraser');
   input.addEventListener('keydown', (e) => {
     if (e.key == 'Enter') {
       const taskText = input.value.trim();
       addTask(taskText);
+      input.value = '';
     }
+  });
+
+  eraser.addEventListener('click', () => {
+    myListTasks.textContent = '';
   });
 
   function addTask(taskText) {
     if (taskText != '') {
-      const taskItem = document.createElement('div');
-      taskItem.textContent = '- ' + taskText;
+      const taskItem = createElement(taskText);
       taskItem.addEventListener('click', () => {
         taskItem.classList.toggle('completed');
       });
       myListTasks.appendChild(taskItem);
-      input.value = '';
     }
+  }
+
+  function createElement(taskText) {
+    const taskItem = document.createElement('div');
+    taskItem.textContent = '- ' + taskText;
+    return taskItem;
   }
 });
