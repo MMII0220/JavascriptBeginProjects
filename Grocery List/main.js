78a809fe86a85eefd9a15cadcff91b66ada96448
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2023-11-01 10:04:58 +0300

    feat: add start file for grocery list

---
+++ Grocery List/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+  const input = document.querySelector('#userInput');
+  const myListTasks = document.querySelector('#groceryItems');
+  input.addEventListener("keydown")
+});
