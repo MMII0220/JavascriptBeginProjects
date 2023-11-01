c5e6664d930905956a2bd4cdaf5e72705e3680c8
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2023-11-01 10:45:18 +0300

    feat: finish v1

--- Grocery List/main.js
+++ Grocery List/main.js
@@ -1,5 +1,22 @@
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
   const input = document.querySelector('#userInput');
   const myListTasks = document.querySelector('#groceryItems');
-  input.addEventListener("keydown")
+  input.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter') {
+      const taskText = input.value.trim();
+      addTask(taskText);
+    }
+  });
+
+  function addTask(taskText) {
+    if (taskText != '') {
+      const taskItem = document.createElement('div');
+      taskItem.textContent = '- ' + taskText;
+      taskItem.addEventListener('click', () => {
+        taskItem.classList.toggle('completed');
+      });
+      myListTasks.appendChild(taskItem);
+      input.value = '';
+    }
+  }
 });
