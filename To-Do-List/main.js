d17af05755332f645e6d46fab1aa9f2134ed9e0e
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2023-11-03 11:32:49 +0300

    feat: finish project v1 with comment

--- To-Do-List/main.js
+++ To-Do-List/main.js
@@ -1,12 +1,43 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const myTaskText = document.querySelector("#input"),
-    btnToAddTask = document.querySelector("#add_item"),
-    myList = document.querySelector("#to_do_list");
+window.addEventListener('DOMContentLoaded', () => {
+  const myTaskText = document.querySelector('#input'),
+    btnToAddTask = document.querySelector('.header_btn-add_task'),
+    myList = document.querySelector('#to_do_list');
 
-  btnToAddTask.addEventListener("click", () => {
-    const myTask = createElement("div");
-    myTask.textContent = myTaskText.value;
+  btnToAddTask.addEventListener('click', handleAddTask); // for btn
+
+  myTaskText.addEventListener('keydown', handleAddTask); // for enter
+
+  function handleAddTask(event) {
+    // after clicking btn or pushing enter add task
+    if (
+      myTaskText.value.trim() != '' &&
+      (event.type === 'click' || (event.type === 'keydown' && event.key === 'Enter'))
+    ) {
+      addTaskList(myTaskText.value);
+      myTaskText.value = '';
+    }
+  }
+
+  function addTaskList(myTaskText) {
+    // creating element and adding task there
+    const myTask = document.createElement('div');
+    const div = document.createElement('div');
+    const text = document.createElement('p');
+    const deleteButton = document.createElement('button');
+
+    myTask.className = 'item';
+    text.textContent = input;
+    deleteButton.textContent = 'delete';
+
+    deleteButton.addEventListener('click', () => {
+      myList.removeChild(myTask);
+    });
+
+    myTask.appendChild(text);
+    myTask.appendChild(div);
+
+    myTask.textContent = myTaskText;
+    myTask.appendChild(deleteButton);
     myList.appendChild(myTask);
-    console.log(myTask);
-  });
+  }
 });
