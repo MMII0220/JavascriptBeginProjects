382fe300b2990f796efbcaddf2f320d0998191ad
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2023-11-15 20:34:37 +0300

    feat: finish code with refactoring

---
+++ Unsplash API/app.js
@@ -0,0 +1,59 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const input = document.getElementById('input'),
+    grid = document.getElementsByClassName('grid')[0];
+
+  window.addEventListener('load', dayNightMode);
+
+  input.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter') {
+      loadImg();
+    }
+  });
+
+  function loadImg() {
+    removeImages();
+    const url =
+      'https://api.unsplash.com/search/photos/?query=' +
+      input.value +
+      '&per_page=9&client_id=ncLZ1ThNf82aFoaHLO9z44zHYJjJEbRciPPboTM-4NM';
+
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert(response.status);
+        }
+      })
+
+      .then((data) => {
+        const imageNodes = [];
+        for (let i = 0; i < data.results.length; i++) {
+          imageNodes[i] = document.createElement('div');
+          imageNodes[i].className = 'img';
+          imageNodes[i].style.backgroundImage = 'url(' + data.results[i].urls.raw + ')';
+          imageNodes[i].addEventListener('dblclick', () => {
+            window.open(data.results[i].links.download, '_blank');
+          });
+          grid.appendChild(imageNodes[i]);
+        }
+      });
+  }
+
+  function removeImages() {
+    grid.innerHTML = '';
+  }
+
+  function dayNightMode() {
+    const date = new Date();
+    const hour = date.getHours();
+
+    if (hour >= 7 && hour <= 19) {
+      document.body.style.backgroundColor = 'whitesmoke';
+      document.body.style.color = 'black';
+    } else {
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+    }
+  }
+});
